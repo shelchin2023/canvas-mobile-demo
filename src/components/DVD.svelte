95196ee22c9b35f8c1e3c271b897ebfe0d0e66bd
shelchin <shelchin@proton.me>
2024-07-02 11:56:42 +0800

    画出一个游戏画面

---
+++ src/components/DVD.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Canvas } from "svelte-canvas";
+  import Background from "./Background.svelte";
+  import Logo from "./Logo.svelte";
+</script>
+
+<Canvas autoplay width="1366" height="768">
+  <Logo />
+  <Background />
+</Canvas>
